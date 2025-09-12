1e5b9967ca9a0f3eccf698a0ce7dadaefce3cfe3
Luan-Galera <luangalera17@gmail.com>
2025-09-12 09:22:26 -0300

    Adiciona gerenciamento de Agenda, Médicos e Pacientes
    Expande o sistema com funcionalidades para gerenciar entidades
    relacionadas à agenda médica, incluindo Médicos, Pacientes,
    Especialidades e Agendas.
    
    Principais mudanças:
    - Adição de controladores e páginas Razor para CRUD de Médicos,
      Pacientes, Especialidades e Agendas.
    - Criação de migrações para configurar tabelas e relacionamentos
      no banco de dados.
    - Atualização do `ApplicationDbContext` para incluir novas
      entidades e mapeamentos.
    - Ajustes no Identity Framework e validações no modelo de dados.
    - Integração das novas funcionalidades ao layout principal.

---
+++ Data/Migrations/20250911175122_Tabelas.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgendaMedica.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Especialidades",
+                columns: table => new
+                {
+                    EspecialidadeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especialidades", x => x.EspecialidadeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    PacienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataNasc = table.Column<DateOnly>(type: "date", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CEP = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prontuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdentityUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.PacienteId);
+                    table.ForeignKey(
+                        name: "FK_Pacientes_AspNetUsers_IdentityUserId",
+                        column: x => x.IdentityUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medico",
+                columns: table => new
+                {
+                    MedicoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CRM = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EspecialidadeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medico", x => x.MedicoId);
+                    table.ForeignKey(
+                        name: "FK_Medico_Especialidades_EspecialidadeId",
+                        column: x => x.EspecialidadeId,
+                        principalTable: "Especialidades",
+                        principalColumn: "EspecialidadeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agendas",
+                columns: table => new
+                {
+                    AgendaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PacienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MedicoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataConsulta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendas", x => x.AgendaId);
+                    table.ForeignKey(
+                        name: "FK_Agendas_Medico_MedicoId",
+                        column: x => x.MedicoId,
+                        principalTable: "Medico",
+                        principalColumn: "MedicoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Agendas_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "PacienteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendas_MedicoId",
+                table: "Agendas",
+                column: "MedicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendas_PacienteId",
+                table: "Agendas",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medico_EspecialidadeId",
+                table: "Medico",
+                column: "EspecialidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_IdentityUserId",
+                table: "Pacientes",
+                column: "IdentityUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendas");
+
+            migrationBuilder.DropTable(
+                name: "Medico");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+
+            migrationBuilder.DropTable(
+                name: "Especialidades");
+        }
+    }
+}
---
+++ Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaMedica.Models
+{
+    public class Agenda
+    {
+        public Guid AgendaId { get; set; } // Chave Primária PK
+        public Guid PacienteId { get; set; } // Chave estrangeira FK
+        public Paciente? Paciente { get; set; } // Objeto referente a chave acima
+        public Guid MedicoId { get; set; } // Chave estrangeira FK
+        public Medico? Medico { get; set; } // Objeto referente a chave acima
+        public DateTime DataConsulta { get; set; }
+        public string? Status { get; set; }
+    }
+}
